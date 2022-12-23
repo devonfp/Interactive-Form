fe9b2ce4fc0e0055caad8dad309b852c80445c2a
Devon Poston <devonposton1@gmail.com>
2022-12-23 08:43:23 -0500

    Made attempts to program job role section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('Test');
+
+
+const input = document.querySelector("input[type='text']");
+const name = input.focus();
+//console.log(name);
+
+
+const roleInput = document.getElementById('other-job-role')
+roleInput.style.display = 'none';
+const roleSelect = document.getElementById('title')
+
+
+
+roleSelect.addEventListener('change', (e) => {
+      
+    const clicked = e.target;
+
+    if (clicked.value === 'OTHER') {
+      roleInput.style.display = 'block';
+}
+});
